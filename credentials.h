9c3e5600bb8b84cb477b87490d13ceda131d78e3
Achilles <github@mail2englisch.de>
2023-11-28 09:20:00 +0100

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -5,13 +5,13 @@
 #include <PubSubClient.h>
 
 // WiFi
-static const char wifi_ssid[]     = "Stroblstr30a";                   // enter your WiFi name here
-static const char wifi_password[] = "aphiTvVWdQLfwBGeM2zy6P";         // enter your WiFi password here
+static const char wifi_ssid[]     = "<your-ssid>";                    // enter your WiFi name here
+static const char wifi_password[] = "<your-pwd>";                     // enter your WiFi password here
 
 // MQTT Broker
-static const char mqtt_broker[]   = "td11a753.ala.us-east-1.emqxsl.com";    // broker address
-static const char mqtt_username[] = "xmas-ticker";                          // username for authentication
-static const char mqtt_password[] = "mxbXE3e16O1exkXFyrQI";                 // password for authentication
+static const char mqtt_broker[]   = "<your-broker>";                  // broker address
+static const char mqtt_username[] = "<your-username>";                // username for authentication
+static const char mqtt_password[] = "<your-user-pwd>";                // password for authentication
 
 static const char mqtt_topic_t[]  = "MatrixDisplay2/text";            // define topic for text
 static const char mqtt_topic_i[]  = "MatrixDisplay2/intensity";       // define topic for intensity
@@ -28,26 +28,7 @@ static const uint8_t mqtt_clen    = 250;
 
 static const char mqtt_cert[]     = \
 "-----BEGIN CERTIFICATE-----\n" \
-"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
-"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
-"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
-"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
-"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
-"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
-"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
-"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
-"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
-"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
-"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
-"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
-"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
-"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
-"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
-"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
-"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
-"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
-"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
-"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+"=\n" \
 "-----END CERTIFICATE-----";
 
 // global data structure
